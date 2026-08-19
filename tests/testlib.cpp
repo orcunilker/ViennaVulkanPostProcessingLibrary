1a6050e1f74ee00bea9701dbc5cd95e3ec8908b6
orcunilked96 <17822754+orcunilker@users.noreply.github.com>
2026-08-19 11:06:54 +0200

    invert.slang schreiben, SPIR-V einbetten

--- tests/testlib.cpp
+++ tests/testlib.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 
+#include "invert_spv.h"
 
 // zeichnet einen Command Buffer fuer einmalige enutzung auf
 VkCommandBuffer beginSingleTime(VkDevice device, VkCommandPool pool) {
